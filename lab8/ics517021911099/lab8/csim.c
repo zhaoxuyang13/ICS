cace45aef69a88169d53ec6da02ac95ccb10a19f
SeanZhao <brokenbones@sjtu.edu.cn>
2019-03-18 23:47:30 +0800

    done first part of the lab8_partA

--- lab8/ics517021911099/lab8/csim.c
+++ lab8/ics517021911099/lab8/csim.c
@@ -1,7 +1,69 @@
+/*
+*   Student Name: Zhao Xuyang
+*   Student ID: 517021911099
+*
+*/
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
 
-int main()
+#define error(s) printf("Error:%s \n", s)
+
+
+typedef enum {FALSE, TRUE } bool_t;
+typedef enum {Inst, Load, Store, Mod} opType_t;
+
+int setBits, E, blockBits, Sets,Blocks;
+char *fileLocation;
+bool_t verbose = FALSE;
+
+extern int optind,opterr,optopt;
+extern char* optarg;
+extern int getopt(int argc, char *const*argv, const char * optstring);
+void parseMainArg(int argc, char *argv[]){
+    char ch;
+    while((ch = getopt(argc,argv,"hvs:E:b:t:"))  != -1)
+    {
+        switch(ch)
+        {
+            case 'h':
+                printf("Some usage info \n");
+                break;
+            case 'v':
+                verbose = TRUE;
+                break;
+            case 's':
+                setBits = atoi(optarg);
+                Sets = 1 << setBits;
+                break;
+            case'E':
+                E = atoi(optarg);
+                break;
+            case 'b':
+                blockBits = atoi(optarg);
+                Blocks = 1 << blockBits;
+                break;
+            case 't':
+                fileLocation = optarg;
+                break;
+            default:
+                printf("unknown option: %c \n", (char)optopt);
+        }
+    }
+}
+
+void initCache(){
+
+}
+void parseFile(){
+
+}
+int main(int argc, char *argv[])
 {
-    printSummary(0, 0, 0);
+     parseMainArg(argc, argv);
+     initCache();
+     parseFile();
     return 0;
 }
