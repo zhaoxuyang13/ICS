ce86373770238db6fbb815895e360a71be95c2e4
zhaoxuyang <771906409@163.com>
2019-07-05 18:12:23 +0800

    rm redundant file

--- lab3/target30/assembly2.s
+++
@@ -1,5 +0,0 @@
-
-mov  $0x556489e0,%rdi
-mov  $0x556489d8,%rsp
-retq
-
--- tut-tools/helloworld.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	//You should add some code here to print "Hello world!" on screen
-	printf("Hello world!\n");
-	return 0;
-}
